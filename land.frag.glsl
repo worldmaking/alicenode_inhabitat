e2499fd584b33c7078e8c3a397b9c985a142d3f1
Graham Wakefield <grrrwaaa@gmail.com>
2018-03-29 10:49:20 -0400

    client updated land.frag.glsl

--- land.frag.glsl
+++ land.frag.glsl
@@ -72,6 +72,13 @@ vec2 pModInterval2(inout vec2 p, vec2 size, vec2 start, vec2 stop) {
 	return c;
 }
 
+// Rotate around a coordinate axis (i.e. in a plane perpendicular to that axis) by angle <a>.
+// Read like this: R(p.xz, a) rotates "x towards z".
+// This is fast if <a> is a compile-time constant and slower (but still practical) if not.
+void pR(inout vec2 p, float a) {
+	p = cos(a)*p + sin(a)*vec2(p.y, -p.x);
+}
+
 float fSphere(vec3 p, float r) {
 	return length(p) - r;
 }

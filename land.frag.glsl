054ece668f7be510f9fb80b2330d03b5df8a99b4
Graham Wakefield <grrrwaaa@gmail.com>
2018-03-29 10:46:46 -0400

    client updated land.frag.glsl

--- land.frag.glsl
+++ land.frag.glsl
@@ -89,7 +89,7 @@ float fCapsule(vec3 p, float r, float c) {
 
 float fScene(vec3 p) {
 	vec3 pc = p;
-	vec2 c = pModInterval2(pc.xz, vec2(0.5), vec2(-32.), vec2(32.));
+	vec2 c = pModInterval2(pc.xz, vec2(0.5), vec2(-64.), vec2(64.));
 	float h = abs(sin(c.y*0.2)*sin(c.x*0.2 + time));
 	float s = fSphere(pc, h); //
 	float b = fBox(pc, vec3(0.3, h, 0.3));

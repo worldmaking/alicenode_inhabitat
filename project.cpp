b453aaf2ea6edd611ed5e88b5a1d80caf85505a8
Graham Wakefield <grrrwaaa@gmail.com>
2018-03-29 09:54:15 -0400

    client updated project.cpp

--- project.cpp
+++ project.cpp
@@ -112,7 +112,7 @@ void onFrame() {
 	
 	// update nav
 	glm::mat4 viewMat = glm::lookAt(
-		glm::vec3(0., 0., 10.), 
+		glm::vec3(0., 20., 20.), 
 		glm::vec3(0., 0., 0.), 
 		glm::vec3(0., 1., 0.));
 	glm::mat4 projMat = glm::perspective(45.0f, 4.f/3.f, 0.1f, 100.0f);

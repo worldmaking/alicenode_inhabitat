b170ce024f5245b909f2877aae16e9bb8627900f
Graham Wakefield <grrrwaaa@gmail.com>
2018-03-29 10:33:00 -0400

    client updated project.cpp

--- project.cpp
+++ project.cpp
@@ -113,7 +113,7 @@ void onFrame() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * NUM_TRIS, &state->translations[0], GL_STATIC_DRAW);
 	
 	// update nav
-	double a = M_PI * t * 0.1;
+	double a = M_PI * t / 30.;
 	glm::mat4 viewMat = glm::lookAt(
 		glm::vec3(16.*sin(a), 10.*fabs(sin(a)), 32.*cos(a)), 
 		glm::vec3(0., 0., 0.), 

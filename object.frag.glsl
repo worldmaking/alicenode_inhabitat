dcf1693c511ca3b7f0d16d71c9ecb8b700ea20ab
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-04 00:52:00 -0400

    client updated project

--- object.frag.glsl
+++ object.frag.glsl
@@ -152,7 +152,6 @@ float fScene(vec3 p) {
 	
 	float se = min(se1, se2);
 	
-	float b1 = fBox(p+vec3(0., 0., -size*0.5), vec3(size, size*0.1, size*0.5));
 	
 	vec3 pc = p+vec3(0., 0., -size*0.5);
 	float a = pModPolar(pc.xy, 18.);

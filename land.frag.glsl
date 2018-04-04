804380093c1b4f25cb54b2ae492de97506ad1bf3
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-04 00:19:04 -0400

    client updated project

--- land.frag.glsl
+++ land.frag.glsl
@@ -223,7 +223,7 @@ void main() {
 		
 		// fog effect:
 		float fogmix = pow(count, 2.);
-		fogmix = max(fogmix, pow(t/maxd, 1.));
+		fogmix = max(fogmix, pow(t/VERYFARAWAY, 1.));
 		color = mix(color, fogcolor, fogmix);
 		
 		FragColor.rgb = color;

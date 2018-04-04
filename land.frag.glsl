e849672b5712bb7e744114e9d6615ac00c4f115d
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-04 00:06:49 -0400

    client updated project

--- land.frag.glsl
+++ land.frag.glsl
@@ -213,7 +213,7 @@ void main() {
 		float cheap_self_occlusion = 1.-pow(count, 0.75);
 		
 		float metallic = acute;
-		color = mix(sky(n), sky(ref), metallic);
+		color = mix(sky(ref), sky(n), metallic);
 		
 		color *= cheap_self_occlusion;
 		

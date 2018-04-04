2944d1717bee40199535569b676217445e45f9eb
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-03 23:45:06 -0400

    client updated project

--- object.frag.glsl
+++ object.frag.glsl
@@ -183,15 +183,15 @@ void main() {
 		
 		float acute = abs(dot(n, ray)); // how much surface faces us
 		float oblique = 1.0 - acute; // how much surface is perpendicular to us
+		color = vec3(oblique);
 		
 		//color += (n*1.)*0.1;
 		//color += mix(color, vec3(0.8)*max(0., dot(n, vec3(1.))), 0.5);
 		
 		
 		float metallic = oblique;
-		color *= mix(sky(n), sky(ref)*2., metallic);
-		//color = vec3(metallic);
-		
+		color *= mix(sky(n)*0.5, sky(ref), metallic);
+		//
 		FragColor.rgb = color;
 		//FragColor.rb += n.xz;
 		

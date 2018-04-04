79766aa5ab1b375206ebd3fc4c55ed6ba88d5e51
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-03 21:35:38 -0400

    client updated project

--- object.frag.glsl
+++ object.frag.glsl
@@ -104,9 +104,10 @@ float fScene(vec3 p) {
 	float s0 = fSphere(p+vec3(0., -size*0.5, size), size*0.5*osc);
 	float s1 = fSphere(p+vec3(0., 0., size*0.25), size*0.75);
 	
-	float se1 = fSphere(p+vec3(size*0.5, size*0.5, size*0.5), size*0.25);
+	float se1 = fSphere(p+vec3( size*0.5, size*0.5, size*0.5), size*0.25);
+	float se2 = fSphere(p+vec3(-size*0.5, size*0.5, size*0.5), size*0.25);
 	
-	float se = se1;
+	float se = min(se1, se2);
 	
 	float b1 = fBox(p+vec3(0., 0., -size*0.5), vec3(size, size*0.1, size*0.5));
 	

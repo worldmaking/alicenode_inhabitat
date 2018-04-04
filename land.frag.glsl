f2a2aabe479ba0ca4370c7a1cdc5e397c2737a9f
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-03 22:46:42 -0400

    client updated project

--- land.frag.glsl
+++ land.frag.glsl
@@ -181,7 +181,7 @@ void main() {
 		
 	} else if (t >= maxd) {
     	// shot through to background
-    	p = ro+gl_DepthRange.far*rd;
+    	p = ro+gl_DepthRange.far*0.9*rd;
     	
     	//FragColor = vec4(clamp(fScene(p), 0., 1.));
     	//discard;

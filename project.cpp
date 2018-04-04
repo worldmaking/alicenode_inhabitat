2657a3660e99a26f8ba86acdd3cedee545fbdb59
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-03 20:50:46 -0400

    objects also have rotation

--- project.cpp
+++ project.cpp
@@ -145,7 +145,7 @@ void onFrame() {
 	for (int i=0; i<NUM_OBJECTS; i++) {
 		Object &o = state->objects[i];
 		o.location = glm::clamp(o.location + glm::ballRand(0.1f), -10.f, 10.f);	
-		o.orientation = glm::normalize(quat_random());
+		o.orientation = safe_normalize(glm::slerp(o.orientation, quat_random(), 0.01f));
 	}
 	
 	// upload GPU;

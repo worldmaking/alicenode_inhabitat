92b0c680e5e905a77afdc80bcc5f37719d19b999
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-04 00:31:00 -0400

    client updated project

--- object.frag.glsl
+++ object.frag.glsl
@@ -98,6 +98,18 @@ float fSphere(vec3 p, float r) {
 	return length(p) - r;
 }
 
+// Rotate around a coordinate axis (i.e. in a plane perpendicular to that axis) by angle <a>.
+// Read like this: R(p.xz, a) rotates "x towards z".
+// This is fast if <a> is a compile-time constant and slower (but still practical) if not.
+void pR(inout vec2 p, float a) {
+	p = cos(a)*p + sin(a)*vec2(p.y, -p.x);
+}
+
+// Shortcut for 45-degrees rotation
+void pR45(inout vec2 p) {
+	p = (p + vec2(p.y, -p.x))*sqrt(0.5);
+}
+
 
 // p is the vec3 position of the surface at the fragment.
 // viewProjectionMatrix would be typically passed in as a uniform

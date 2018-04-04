f45fbe5ca0a2099aa7c1084022f8f3ad33a25f04
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-03 23:15:26 -0400

    client updated project

--- land.frag.glsl
+++ land.frag.glsl
@@ -178,7 +178,7 @@ void main() {
 	//FragColor = vec4(texCoord, 0.5, 1.);
 	//FragColor = vec4(rd, 0.);
 	//FragColor = texture(tex, texCoord);
-	color = vec3(mix(0.5, 0., count));
+	color = vec3(mix(1., 0., count));
 	
 	vec3 fogcolor = sky(rd);
 	

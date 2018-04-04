eb53f2bc5acadcbc2d1b78758562a54f2cd64ba5
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-03 21:30:16 -0400

    client updated project

--- project.cpp
+++ project.cpp
@@ -147,7 +147,7 @@ void onFrame() {
 		Object &o = state->objects[i];
 		o.location = wrap(o.location + quat_uf(o.orientation)*0.05f, glm::vec3(-20.f, 0.f, -20.f), glm::vec3(20.f, 10.f, 20.f));	
 		//o.location = glm::clamp(o.location + glm::ballRand(0.1f), glm::vec3(-20.f, 0.f, -20.f), glm::vec3(20.f, 10.f, 20.f));	
-		o.orientation = safe_normalize(glm::slerp(o.orientation, quat_random(), 0.05f));
+		o.orientation = safe_normalize(glm::slerp(o.orientation, o.orientation * quat_random(), 0.05f));
 	}
 	
 	// upload GPU;

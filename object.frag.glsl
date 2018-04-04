f6fac8600dceacffc4c0abed7549a79818f0a85f
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-04 00:40:39 -0400

    client updated project

--- object.frag.glsl
+++ object.frag.glsl
@@ -156,9 +156,9 @@ float fScene(vec3 p) {
 	
 	vec3 pc = p+vec3(0., 0., -size*0.5);
 	
-	float a = pModPolar(pc.xy, 16);
+	float a = pModPolar(pc.xy, 16.);
 	
-	pR(pc.xz, 0.5*sin(time * 14. + a * PI));
+	pR(pc.xz, 0.5*sin(time * 14. + a*PI/16.));
 	
 	
 	float c1 = fCylinder(pc.zxy, size*.05, size*0.5);

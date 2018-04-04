f719f303f4e368b388d6a90ae7bea407f43c97fd
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-04 00:26:36 -0400

    client updated project

--- object.frag.glsl
+++ object.frag.glsl
@@ -126,7 +126,7 @@ float fScene(vec3 p) {
 	
 	float b1 = fBox(p+vec3(0., 0., -size*0.5), vec3(size, size*0.1, size*0.5));
 	
-	float c1 = fCylinder(p, 0.5, 0.2);
+	float c1 = fCylinder(p, size, 0.2);
 	
 	float z = max(s1, -s0); 
 	//z = min(z, b1);

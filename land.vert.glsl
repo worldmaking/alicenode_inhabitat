0bfffc017274f13397162a84e35f028849eafdaa
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-03 22:54:09 -0400

    client updated project

--- land.vert.glsl
+++ land.vert.glsl
@@ -6,7 +6,7 @@ layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec2 aTexCoord;
 
 out vec2 texCoord;
-out vec3 ray, origin;
+out vec3 ray, origin, eyepos;
 
 void main() {
 	gl_Position = vec4(aPos, 0., 1.0);
@@ -24,4 +24,6 @@ void main() {
 	// in practice we might want to add near-clip... 
 	origin = near3;
 	
+	// derive eye location in world space from current (model)view matrix:
+    eyepos = -(uViewMatrix[3].xyz)*mat3(uViewMatrix);
 }

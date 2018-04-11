e3daa8df7883408ae5a2320f0dd3c5bf51544730
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-11 14:31:47 -0400

    client updated project

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @if "%_echo%"=="" echo off 
 @setlocal
+set ALICE_DIR=%1
 SET VSCMD_START_DIR=%cd%
 SET VCVARS64=VC\Auxiliary\Build\vcvars64.bat
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\%VCVARS64%" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\%VCVARS64%"
@@ -7,10 +8,11 @@ if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\%VCVARS6
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\%VCVARS64%" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\%VCVARS64%"
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\%VCVARS64%" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\%VCVARS64%"
 
+
 REM compile & link:
 cl /nologo /LD /W3 /EHsc /O2 ^
-/I ../server/include ^
-../server/alice.lib ^
+/I %ALICE_DIR%\include ^
+%ALICE_DIR%\alice.lib ^
 project.cpp ^
 user32.lib kernel32.lib shell32.lib gdi32.lib opengl32.lib 
 

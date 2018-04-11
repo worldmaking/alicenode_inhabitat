2b15558e04d7236f71b9c2f52ed71210b5753752
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-11 14:40:30 -0400

    restored the project state

--- project.cpp
+++ project.cpp
@@ -5,8 +5,7 @@
 #include "alice.h"
 #include "state.h"
 
-
-Shader * shader_test;
+Shader * objectShader;
 unsigned int VAO;
 unsigned int VBO;
 unsigned int instanceVBO;
@@ -14,14 +13,65 @@ unsigned int instanceVBO;
 Shader * landShader;
 QuadMesh quadMesh;
 
+float vertices[] = {
+    -1.0f,-1.0f,-1.0f, 
+    -1.0f,-1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f, 
+    
+    1.0f, 1.0f,-1.0f, 
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f, 
+    
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+    
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    
+    -1.0f, 1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+    
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    
+    1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f,
+    
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f
+};
+
+State * state;
 Mmap<State> statemap;
 
 
 void onUnloadGPU() {
 	// free resources:
-	if (shader_test) {
-		delete shader_test;
-		shader_test = 0;
+	if (objectShader) {
+		delete objectShader;
+		objectShader = 0;
 	}	
 	
 	quadMesh.dest_closing();
@@ -48,9 +98,9 @@ void onReloadGPU() {
 	
 	quadMesh.dest_changed();
 	
-	shader_test = Shader::fromFiles("test.vert.glsl", "test.frag.glsl");
-	if (!shader_test) return;
-	console.log("shader loaded %p = %d", shader_test, (int)shader_test->program);
+	objectShader = Shader::fromFiles("object.vert.glsl", "object.frag.glsl");
+	if (!objectShader) return;
+	console.log("shader loaded %p = %d", objectShader, (int)objectShader->program);
 	
 	// define the VAO 
 	// (a VAO stores attrib & buffer mappings in a re-usable way)
@@ -68,151 +118,104 @@ void onReloadGPU() {
 
 	glGenBuffers(1, &instanceVBO);
 	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * NUM_TRIS, &state->translations[0], GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Object) * NUM_OBJECTS, &state->objects[0], GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(2);
 	// attr location, element size & type, normalize?, source stride & offset
-	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, sizeof(Object), (void*)offsetof(Object, location));
 	// mark this attrib as being per-instance	
 	glVertexAttribDivisor(2, 1);  
+	
+	glEnableVertexAttribArray(3);
+	// attr location, element size & type, normalize?, source stride & offset
+	glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(Object), (void*)offsetof(Object, orientation));
+	// mark this attrib as being per-instance	
+	glVertexAttribDivisor(3, 1);  
+	
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 }
 
 void onFrame() {
 
 	double t = Alice::Instance().t;
-	
+
+	// update simulation:
 	for (int i=0; i<NUM_OBJECTS; i++) {
 		Object &o = state->objects[i];
-		o.location = wrap(o.location + quat_uf(o.orientation)*0.05f, glm::vec3(-64.f, 0.f, -64.f), glm::vec3(64.f, 10.f, 64.f));	
+		o.location = wrap(o.location + quat_uf(o.orientation)*0.05f, glm::vec3(-20.f, 0.f, -20.f), glm::vec3(20.f, 10.f, 20.f));	
 		//o.location = glm::clamp(o.location + glm::ballRand(0.1f), glm::vec3(-20.f, 0.f, -20.f), glm::vec3(20.f, 10.f, 20.f));	
-		o.orientation = safe_normalize(glm::slerp(o.orientation, o.orientation * quat_random(), 0.025f));
-		
+		o.orientation = safe_normalize(glm::slerp(o.orientation, o.orientation * quat_random(), 0.05f));
+
 	}
-	
+
 	// upload GPU;
 	glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * NUM_TRIS, &state->translations[0], GL_STATIC_DRAW);
-	
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Object) * NUM_OBJECTS, &state->objects[0], GL_STATIC_DRAW);
+
 	// update nav
-	double a = M_PI * t / 3.;
+	double a = M_PI * t / 30.;
 	glm::mat4 viewMat = glm::lookAt(
-		glm::vec3(16.*sin(a), 10.*(1.+cos(a)), 32.*cos(a)), 
-		glm::vec3(0., 0., 0.), 
-		glm::vec3(0., 1., 0.));
+	glm::vec3(16.*sin(a), 10.*(1.2+cos(a)), 32.*cos(a)), 
+	glm::vec3(0., 0., 0.), 
+	glm::vec3(0., 1., 0.));
 	glm::mat4 projMat = glm::perspective(45.0f, 4.f/3.f, 0.1f, 100.0f);
 	glm::mat4 viewProjMat = projMat * viewMat;
 	glm::mat4 viewProjMatInverse = glm::inverse(viewProjMat);
-	
+
 	// start rendering:
-	glDisable(GL_DEPTH_TEST);
-	
+
 	landShader->use();
-    landShader->uniform("time", t);
-    landShader->uniform("uViewMatrix", viewMat);
-    landShader->uniform("uViewProjectionMatrix", viewProjMat);
-    landShader->uniform("uViewProjectionMatrixInverse", viewProjMatInverse);
+	landShader->uniform("time", t);
+	landShader->uniform("uViewProjectionMatrix", viewProjMat);
+	landShader->uniform("uViewProjectionMatrixInverse", viewProjMatInverse);
 	quadMesh.draw();
 
-	glEnable(GL_DEPTH_TEST);
-	
 	objectShader->use();
-    objectShader->uniform("time", t);
-    objectShader->uniform("uViewMatrix", viewMat);
-    objectShader->uniform("uViewProjectionMatrix", viewProjMat);
-    objectShader->uniform("uViewProjectionMatrixInverse", viewProjMatInverse);
-    
-    glBindVertexArray(VAO);
-    // offset, vertex count
-    //glDrawArrays(GL_TRIANGLES, 0, 3);
-    // draw instances:
-    glDrawArraysInstanced(GL_TRIANGLES, 0, 3, NUM_TRIS);  
-}
+	objectShader->uniform("time", t);
+	objectShader->uniform("uViewMatrix", viewMat);
+	objectShader->uniform("uViewProjectionMatrix", viewProjMat);
+	objectShader->uniform("uViewProjectionMatrixInverse", viewProjMatInverse);
+
+	glBindVertexArray(VAO);
+	// offset, vertex count
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
+	// draw instances:
+	glDrawArraysInstanced(GL_TRIANGLES, 0, sizeof(vertices) / (sizeof(float) * 3), NUM_OBJECTS);   
+	}
 
 
 void state_initialize() {
-	for (int i=0; i<NUM_TRIS; i++) {
-		state->translations[i] = glm::diskRand(1.f);
+	for (int i=0; i<NUM_OBJECTS; i++) {
+		state->objects[i].location = glm::ballRand(1.f);
 	}
-}
-
-typedef double t_sample;
-
-inline t_sample cubic_interp(t_sample a, t_sample w, t_sample x, t_sample y, t_sample z) {
-	const t_sample a2 = a*a;
-	const t_sample f0 = z - y - w + x;
-	const t_sample f1 = w - x - f0;
-	const t_sample f2 = y - w;
-	const t_sample f3 = x;
-	return (f0*a*a2 + f1*a2 + f2*a + f3);
-}
-
-inline t_sample altcubic(t_sample a, t_sample w, t_sample x, t_sample y, t_sample z) {
-	const t_sample f0 = 1. + a; 
-	
-	const t_sample f1 = 1. - a; 
-	
-	const t_sample f2 = 2. - a; 
-	
-	const t_sample f3 = f1 * f2; 
-	
-	const t_sample f4 = f0 * a;
-	
-	const t_sample fw = -.1666667 * f3 * a; 
-	
-	const t_sample fx = .5 * f0 * f3; 
-	
-	const t_sample fy = .5 * f4 * f2; 
-	
-	const t_sample fz = -.1666667 * f4 * f1;
-	
-	return w * fw + x * fx + y * fy + z * fz;
-}
-
-// Breeuwsma catmull-rom spline interpolation
-inline t_sample spline_interp(t_sample a, t_sample w, t_sample x, t_sample y, t_sample z) {
-	const t_sample a2 = a*a;
-	const t_sample f0 = t_sample(-0.5)*w + t_sample(1.5)*x - t_sample(1.5)*y + t_sample(0.5)*z;
-	const t_sample f1 = w - t_sample(2.5)*x + t_sample(2)*y - t_sample(0.5)*z;
-	const t_sample f2 = t_sample(-0.5)*w + t_sample(0.5)*y;
-	return (f0*a*a2 + f1*a2 + f2*a + x);
-}
-
-inline t_sample spline_harker(t_sample a, t_sample w, t_sample x, t_sample y, t_sample z)
-{
-    // N.B. - this is currently a high-quality cubic hermite
-    const t_sample f0 = t_sample(0.5) * (z - w) + t_sample(1.5) * (x - y);
-    const t_sample f1 = w - t_sample(2.5) * x + y + y - t_sample(0.5) * z;
-    const t_sample f2 = t_sample(0.5) * (y - w);
-    
-    return (((f0 * a + f1) * a + f2) * a + x);
-    //const t_sample a2 = a*a;
-	//return (f0*a*a2 + f1*a2 + f2*a + x);
 }
 
 extern "C" {
     AL_EXPORT int onload() {
     	
-    	Alice& alice = Alice::Instance();
-    
-    	console.log("onload");
+		Alice& alice = Alice::Instance();
+
+		console.log("onload");
 		console.log("sim alice %p", &alice);
 
-    	// import/allocate state
-    	state = statemap.create("state.bin", true);
-    	console.log("sim state %p should be size %d", state, sizeof(State));
-    	//state_initialize();
-    	console.log("initialized");
-    	
-    	// allocate on GPU:
-    	onReloadGPU();
-    	
-    	// register event handlers 
+		// import/allocate state
+		state = statemap.create("state.bin", true);
+		console.log("sim state %p should be size %d", state, sizeof(State));
+		//state_initialize();
+		console.log("initialized");
+
+		// allocate on GPU:
+		onReloadGPU();
+
+		// register event handlers 
 		alice.onFrame.connect(onFrame);
 		alice.onReloadGPU.connect(onReloadGPU);
 
-        return 0;
+		//test();
+
+		return 0;
     }
     
     AL_EXPORT int onunload() {

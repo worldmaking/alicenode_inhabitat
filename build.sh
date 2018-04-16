69f092f7549fb2581057d4dafb7a4893712b6ecd
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-16 15:12:18 -0400

    bash build with default args

--- build.sh
+++ build.sh
@@ -1,1 +1,11 @@
-clang++ -shared -O3 -Wall -std=c++11 -stdlib=libc++ -fexceptions -I$1/include -L$1/lib/osx -undefined dynamic_lookup project.cpp -o project.dylib
+#!/usr/bin/env bash
+if [ $# -ge 1 ]
+then
+    echo path
+	ALICEPATH=$1
+else
+	ALICEPATH="../alicenode"
+    echo path to alicenode not specified, assuming: $ALICEPATH
+fi
+
+clang++ -shared -O3 -Wall -std=c++11 -stdlib=libc++ -fexceptions -I$ALICEPATH/include -L$ALICEPATH/lib/osx -undefined dynamic_lookup project.cpp -o project.dylib

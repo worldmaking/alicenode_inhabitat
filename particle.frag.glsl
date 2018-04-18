1a5a3a2faa37e126832d5957f51873073dbffdbf
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 13:02:50 -0400

    client updated project

--- particle.frag.glsl
+++ particle.frag.glsl
@@ -89,7 +89,7 @@ void main() {
 	vec3 billboard_position = point_position + billboard;
 	// use this to compute the ray direction from the eye:
 	vec3 rd = normalize(billboard_position - eye_position);
-	vec3 rd1 = normalize(sphere_position - eye_position);
+	vec3 rd1 = normalize(box_position - eye_position);
 	vec3 rdiff = rd1 - rd;
 	// the ray origin (relative to the particle location)
 	// is computed by stepping back along the ray

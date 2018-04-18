e9cc5ea088be9b17dcf0adebfe24cf74e0a227f9
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 17:21:17 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -35,32 +35,6 @@ void main() {
 		float metallic = oblique;
 		color.rgb *= mix(sky(ref), sky(normal), metallic);
 			
-		/*
-
-			// normal in object space:
-			vec3 no = normal4(p, .01);
-			// normal in world space
-			vec3 n = quat_rotate(world_orientation, no);
-			// ray direction in world space
-			vec3 ray = quat_rotate(world_orientation, rd);
-			
-			// reflection vector 
-			vec3 ref = reflect(ray, n);
-			
-			float acute = abs(dot(n, ray)); // how much surface faces us
-			float oblique = 1.0 - acute; // how much surface is perpendicular to us
-			color = vec3(oblique);
-			
-			//color += (n*1.)*0.1;
-			//color += mix(color, vec3(0.8)*max(0., dot(n, vec3(1.))), 0.5);
-			
-			float cheap_self_occlusion = 1.-pow(count, 0.75);
-			
-			
-			
-			color *= cheap_self_occlusion;
-		*/
-		
 		// fog effect:
 		vec3 fogcolor = sky(rd);
 		float fogmix = clamp(length(position)/far_clip, 0., 1.);

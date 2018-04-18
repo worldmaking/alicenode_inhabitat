896c6b1e79697048b1d6d333807449c3b4c5791f
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 16:53:39 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -12,6 +12,6 @@ void main() {
 	vec3 normal = texture(gNormal, texCoord).xyz;
 	vec3 position = texture(gPosition, texCoord).xyz;
 	FragColor.rgb = vec3(texCoord, 0.5);
-	FragColor.rgb = normal.xyz;
+	//FragColor.rgb = normal.xyz;
 	//FragColor.rgb = position.xyz;
 }

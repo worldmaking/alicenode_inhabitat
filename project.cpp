341f1ee9a38d2656c07c1b1d1a013932b1cefdbc
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 14:01:57 -0400

    client updated project

--- project.cpp
+++ project.cpp
@@ -317,11 +317,12 @@ void onFrame(uint32_t width, uint32_t height) {
 
 			glm::vec3 flow;
 			fluid.velocities.front().read_interp(o.location, &flow.x);
+			flow *= 0.5f;
 
 			glm::vec3 push = quat_uf(o.orientation) * 0.2f;
 			fluid.velocities.front().add(o.location, &push.x);
 
-			o.location = wrap(o.location + flow * 1.f, glm::vec3(-20.f, 0.f, -20.f), glm::vec3(20.f, 10.f, 20.f));
+			o.location = wrap(o.location + flow, glm::vec3(-20.f, 0.f, -20.f), glm::vec3(20.f, 10.f, 20.f));
 
 
 

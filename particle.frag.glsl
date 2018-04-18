e21ad90c1785c00c45b8d18365221874f4fb2033
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 13:30:35 -0400

    client updated project

--- particle.frag.glsl
+++ particle.frag.glsl
@@ -183,7 +183,7 @@ void main() {
 		float metallic = 1.;//acute;
 		color = mix(sky(n), sky(ref), metallic);
 		
-		color *= cheap_self_occlusion;
+		//color *= cheap_self_occlusion;
 		
 		// fog effect:
 		vec3 fogcolor = sky(ray);

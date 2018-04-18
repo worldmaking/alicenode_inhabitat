5dbecb0b4e078a15cb9a34344210d51a96a04297
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 17:22:56 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -29,7 +29,7 @@ void main() {
 		float acute = abs(dot(normal, rd)); // how much surface faces us
 		float oblique = 1.0 - acute; // how much surface is perpendicular to us
 
-		color *= acute;	
+		//color *= acute;	
 
 		//float metallic = acute;
 		float metallic = oblique;

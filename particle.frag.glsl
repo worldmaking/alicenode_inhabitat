eeda83c138c13a3be5aad441cd86e93fdb3b3300
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 13:06:08 -0400

    client updated project

--- particle.frag.glsl
+++ particle.frag.glsl
@@ -95,7 +95,7 @@ void main() {
 	rd = rd;
 	// the ray origin (relative to the particle location)
 	// is computed by stepping back along the ray
-	vec3 ro = billboard - rd * world_scale;
+	vec3 ro = billboard - rd * world_scale*sphere.z;
 	
 	float maxd = 2. * world_scale;
 	float d = maxd;

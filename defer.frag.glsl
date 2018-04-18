8a07512b33e938887fc4ea49323452bd3ce0de0e
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 17:04:21 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -7,6 +7,7 @@ uniform sampler2D gPosition;
 in vec2 texCoord;
 out vec4 FragColor;
 
+
 vec3 sky(vec3 dir) {
 	vec3 n = dir*0.5+0.5;
 	n.g = mix(n.g, n.r, 0.5);
@@ -25,9 +26,9 @@ void main() {
 	vec3 color = vec3(0.5);
 
 	// fog effect:
-	vec3 fogcolor = sky(ray);
-	float fogmix = length(world_position)/VERYFARAWAY;
-	color = mix(color, fogcolor, fogmix);
+	//vec3 fogcolor = sky(ray);
+	//float fogmix = length(world_position)/VERYFARAWAY;
+	//color = mix(color, fogcolor, fogmix);
 
 	FragColor.rgb = color;
 }

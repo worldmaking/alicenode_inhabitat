c4fba344ad2c0b10e746ce512c0fda39c6799db8
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 13:29:36 -0400

    client updated project

--- particle.frag.glsl
+++ particle.frag.glsl
@@ -135,7 +135,7 @@ void main() {
 	float precis = 0.001;
 	float count = 0.;
 	float t = 0.;
-	#define MAX_STEPS 32
+	#define MAX_STEPS 8
 	float STEP_SIZE = 1./float(MAX_STEPS);
 	for( int i=0; i<MAX_STEPS; i++ ) {
 	

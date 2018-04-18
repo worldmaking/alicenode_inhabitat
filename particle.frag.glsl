93223e9142bc7f76aa24b5f0618dcf27a1f237b3
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 13:24:53 -0400

    client updated project

--- particle.frag.glsl
+++ particle.frag.glsl
@@ -7,6 +7,12 @@ in vec3 point_position, eye_position;
 in float world_scale;
 out vec4 FragColor;
 
+vec3 sky(vec3 dir) {
+	vec3 n = dir*0.5+0.5;
+	n.g = mix(n.g, n.r, 0.5);
+	return mix(n, vec3(1.), 0.75);
+}
+
 #define PI 3.14159265359
 
 //	q must be a normalized quaternion

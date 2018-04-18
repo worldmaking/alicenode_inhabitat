ed2e230c9fd8f53346a6c834b19c072ab8e31181
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 16:13:24 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -1,9 +1,13 @@
 #version 330 core
-out vec4 FragColor;
-in vec2 texCoord;
-uniform sampler2D tex;
 
-				void main() {
+uniform sampler2D gColor;
+uniform sampler2D gNormal;
+uniform sampler2D gPosition;
+
+in vec2 texCoord;
+out vec4 FragColor;
+
+void main() {
 	//FragColor = vec4(texCoord, 0.5, 1.);
-	FragColor = texture(tex, texCoord);
+	FragColor = texture(gColor, texCoord);
 }

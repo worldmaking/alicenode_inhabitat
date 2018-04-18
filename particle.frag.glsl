9f717f1b691257a29459c70f89839da10823d618
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 12:45:28 -0400

    client updated project

--- particle.frag.glsl
+++ particle.frag.glsl
@@ -78,8 +78,7 @@ void main() {
 	vec3 vertex_position = point_position + world_scale*spherenormal;
 
 	vec3 offset = world_scale * mat3(uViewMatrixInverse) * vec3(snorm, 0.);
-	// 
-	vec3 vertex_position = point_position + world_scale*spherenormal;
+	
 	vec3 rd = normalize(vertex_position - eye_position);
 	vec3 ro = offset - rd * world_scale;
 

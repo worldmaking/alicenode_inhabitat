bd371c8931a3e8ddc0914436e521ce38bf8d1845
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 17:07:16 -0400

    client updated project

--- defer.vert.glsl
+++ defer.vert.glsl
@@ -1,10 +1,15 @@
 #version 330 core
+uniform vec3 eye;
+
 layout (location = 0) in vec2 aPos;
 layout (location = 1) in vec2 aTexCoord;
 
 out vec2 texCoord;
+out vec3 ray_direction;
 
 void main() {
 	gl_Position = vec4(aPos, 0., 1.0);
 	texCoord = aTexCoord;
+
+
 }

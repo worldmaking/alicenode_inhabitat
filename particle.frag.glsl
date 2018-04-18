88746b4c47c94e2b391bcd196a1f48ad2bf90eee
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 14:04:46 -0400

    client updated project

--- particle.frag.glsl
+++ particle.frag.glsl
@@ -211,8 +211,8 @@ void main() {
 		
 		vec3 color;
 		
-		color = 1.-sky(-n); 
-		color = 1.-sky(-ref);
+		color = sky(-n); 
+		color = sky(-ref);
 		
 		// fog effect:
 		vec3 fogcolor = sky(ray);

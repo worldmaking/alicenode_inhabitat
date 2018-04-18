304ae511fa7c02472f3f7162de585067b0bd5c01
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-18 11:47:22 -0400

    client updated project

--- project.cpp
+++ project.cpp
@@ -13,7 +13,7 @@ unsigned int objectInstanceVBO;
 
 unsigned int particlesVAO;
 unsigned int particlesVBO;
-float particleSize = 0.03f;
+float particleSize = 0.3f;
 
 Shader * particleShader;
 Shader * landShader;

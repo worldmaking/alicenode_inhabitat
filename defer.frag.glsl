632d1ebd9b67478bc691bde8967e9b7cbeea755d
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-19 11:56:13 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -87,7 +87,7 @@ void main() {
 	// except, that we should be ignoring them if they are too large
 
 	// set peak around 0.1, null around 1.
-	color.r = smoothstep(0.0, 0.01, abs(diffl));
+	color.r = bump1(diff1, 0.1, 1.);
 	
 	//color = basecolor.rgb;
 	

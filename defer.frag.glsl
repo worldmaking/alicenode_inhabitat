d4be8d0814a9add8b7f620523189cc396af7ed3b
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-19 11:46:36 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -87,7 +87,7 @@ void main() {
 	// except, that we should be ignoring them if they are too large
 
 
-	color.r = bump(texCoord.x, 0.2, 0.4);
+	color.r = bump(texCoord.x, sides.x, 0.4);
 	
 	//color = basecolor.rgb;
 	

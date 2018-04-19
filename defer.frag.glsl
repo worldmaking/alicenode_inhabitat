173afe377d16cbee84d3a35d3d53c42a7960715f
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-19 11:09:38 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -61,7 +61,7 @@ void main() {
 	float depthn = (depthl + depthr + depthu + depthd) / 4.;
 
 	// ao should kick in if the near pixels are closer:
-	float aol = max(depthl - depth, 0.);
+	float aol = depthl; //max(depthl - depth, 0.);
 
 	vec3 color = basecolor.rgb;
 	

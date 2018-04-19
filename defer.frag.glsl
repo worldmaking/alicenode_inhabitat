85720271cff24f13e4cc647669baf302cdbfd1e4
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-19 11:17:39 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -112,7 +112,7 @@ void main() {
 
 	// edge finding by depth difference:
 	float edges = clamp(depth-depthn, 0., 1.)*.5;
-	color.rgb = vec3(aol);
+	color.rgb = vec3(rayDotN);
 	
 
 	FragColor.rgb = color;	

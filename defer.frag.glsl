cfcc762605208b5c91028cc666acef7837120a93
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-19 11:24:56 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -78,7 +78,7 @@ void main() {
 	float expectedDepthl = depth + rayDotN*sides.x;
 
 
-	color.r = (depth - expectedDepthl)*1000.;
+	color.r = (depth - expectedDepthl)*100.;
 	
 	//color = basecolor.rgb;
 	

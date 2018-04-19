9aaf30d4baf028bccadd2366f3b4035e75027aa4
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-19 11:44:09 -0400

    client updated project

--- defer.frag.glsl
+++ defer.frag.glsl
@@ -84,7 +84,7 @@ void main() {
 	// except, that we should be ignoring them if they are too large
 
 
-	color.r = texCoord.x;
+	color.r = smoothstep(0.2, 0.3, texCoord.x);
 	
 	//color = basecolor.rgb;
 	

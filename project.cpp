19fcde386d969d5a1729b54000fdfde945b8f141
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-20 15:02:02 -0400

    client updated project

--- project.cpp
+++ project.cpp
@@ -16,7 +16,7 @@ struct GBuffer {
 	unsigned int textures[numBuffers];
 	unsigned int attachments[numBuffers];
 
-	glm::ivec2 dim = glm::ivec2(512, 512);
+	glm::ivec2 dim = glm::ivec2(64, 64);
 	
 
 	void dest_changed() {

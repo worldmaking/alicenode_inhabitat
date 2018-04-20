98c5cc3b7d8e220959925b2caec1b38e0d9f6c03
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-20 14:59:11 -0400

    client updated project

--- project.cpp
+++ project.cpp
@@ -453,20 +453,18 @@ void onFrame(uint32_t width, uint32_t height) {
 		// update simulation:
 		fluid_update();
 
-		if (1) {
-			for (int i=0; i<NUM_PARTICLES; i++) {
-				Particle &o = state->particles[i];
+		for (int i=0; i<NUM_PARTICLES; i++) {
+			Particle &o = state->particles[i];
 
-				glm::vec3 flow;
-				fluid.velocities.front().read_interp(o.location, &flow.x);
-				
-				glm::vec3 noise;// = glm::sphericalRand(0.02f);
+			glm::vec3 flow;
+			fluid.velocities.front().read_interp(o.location, &flow.x);
+			
+			glm::vec3 noise;// = glm::sphericalRand(0.02f);
 
-				o.location = wrap(
-					o.location + flow + noise, 
-					glm::vec3(-20.f, 0.f, -20.f), 
-					glm::vec3(20.f, 10.f, 20.f));
-			}
+			o.location = wrap(
+				o.location + flow + noise, 
+				glm::vec3(-20.f, 0.f, -20.f), 
+				glm::vec3(20.f, 10.f, 20.f));
 		}
 
 		for (int i=0; i<NUM_OBJECTS; i++) {
@@ -497,10 +495,10 @@ void onFrame(uint32_t width, uint32_t height) {
 
 	// upload GPU;
 	glBindBuffer(GL_ARRAY_BUFFER, objectInstanceVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Object) * NUM_OBJECTS, &state->objects[0], GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(Object) * NUM_OBJECTS, &state->objects[0], GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ARRAY_BUFFER, particlesVBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Particle) * NUM_PARTICLES, &state->particles[0], GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(Particle) * NUM_PARTICLES, &state->particles[0], GL_STATIC_DRAW);
 
 	// update nav
 	double a = M_PI * t / 30.;

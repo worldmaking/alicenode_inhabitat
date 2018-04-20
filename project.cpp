c0e9b250921f156cf1edccc92d9f0afb3e031d04
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-20 17:00:05 -0400

    client updated project

--- project.cpp
+++ project.cpp
@@ -341,16 +341,15 @@ void onReloadGPU() {
 	fbo.dest_changed();
 	gBuffer.dest_changed();
 
-	
 	{
 		glGenTextures(1, &colorTex);
 		glBindTexture(GL_TEXTURE_2D, colorTex);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);		
-		
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);	
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);  
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);  
 		//glTexParameteri( GL_TEXTURE_3D, GL_GENERATE_MIPMAP, GL_TRUE ); 
+		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
 	{

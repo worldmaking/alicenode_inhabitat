1df64f48d2594626c505e9adb72afac12be4965f
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-20 20:34:42 -0400

    client updated project

--- land.frag.glsl
+++ land.frag.glsl
@@ -120,7 +120,7 @@ float fScene(vec3 p) {
 	
 	vec3 pc = p;
 	//vec2 c = pModInterval2(pc.xz, vec2(1.), vec2(-32.), vec2(32.));
-	vec2 c = pMod2(pc.xz, vec2(025));
+	vec2 c = pMod2(pc.xz, vec2(0.25));
 	float h = abs(sin(c.y*0.2)*sin(c.x*0.2));
 	
 	pR(pc.yx, h*0.2*sin(c.y+time*1.3));

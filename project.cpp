37b363f0134e50ae4f2cfadca357e2b7e167fe0f
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-20 20:32:13 -0400

    client updated project

--- project.cpp
+++ project.cpp
@@ -530,7 +530,7 @@ void onFrame(uint32_t width, uint32_t height) {
 			glm::vec3 flow;
 			fluid.velocities.front().read_interp(world2fluid * o.location, &flow.x);
 			
-			float creature_speed = 0.125f*(float)alice.dt;
+			float creature_speed = 0.05f*(float)alice.dt;
 			glm::vec3 push = quat_uf(o.orientation) * creature_speed;
 			fluid.velocities.front().add(world2fluid * o.location, &push.x);
 

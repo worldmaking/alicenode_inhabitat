68870b1f7fab8612ce8921694b955c158fcf3a63
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-20 20:24:23 -0400

    client updated project

--- project.cpp
+++ project.cpp
@@ -117,8 +117,8 @@ float particleSize = 1.f/256;
 float near_clip = 0.1f;
 float far_clip = 24.f;
 
-glm::vec3 world_min(-4.f, 0.f, -4.f);
-glm::vec3 world_max(4.f, 4.f, 4.f);
+glm::vec3 world_min(-4.f, 0.f, 0.f);
+glm::vec3 world_max(4.f, 4.f, 8.f);
 float world2fluid = 8.f; 
 
 Shader * particleShader;

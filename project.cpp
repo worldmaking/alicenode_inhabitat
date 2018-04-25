2b0a2465b67e7ca122abc14b3296b8db2f8b4d3d
Graham Wakefield <grrrwaaa@gmail.com>
2018-04-25 18:01:47 -0400

    client updated project

--- project.cpp
+++ project.cpp
@@ -544,7 +544,9 @@ void onFrame(uint32_t width, uint32_t height) {
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 			if (alice.hmd->connected) {
-				
+				const Hmd& vive = *alice.hmd;
+				viewMat = glm::inverse(vive.m_mat4viewEye[eye]) * glm::mat4_cast(glm::inverse(vive.mTrackedQuat)) * glm::translate(glm::mat4(1.f), -vive.mTrackedPosition);
+				projMat = glm::frustum(vive.frustum[eye].l, vive.frustum[eye].r, vive.frustum[eye].b, vive.frustum[eye].t, vive.frustum[eye].n, vive.frustum[eye].f);
 			} else {
 				viewMat = glm::lookAt(
 					glm::vec3(16.*sin(a), 10.*(1.2+cos(a)), 32.*cos(a)), 

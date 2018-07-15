5df39d5bd7666b8103768018f1c9e3c3cf4d1a28
Graham Wakefield <grrrwaaa@gmail.com>
2018-07-15 14:18:29 +0900

    calibration

--- lmesh.frag.glsl
+++ lmesh.frag.glsl
@@ -124,7 +124,7 @@ void main() {
 		
 	}*/
 
-	vec3 island = vec3(120., 0., 345.);
+	vec3 island = vec3(120., 0., 75.);
 	if (abs(island.x - position.x) < 5.
 	|| abs(island.z - position.z) < 5.) color = vec3(1, 0, 0);
 

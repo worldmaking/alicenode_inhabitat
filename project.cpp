2bdec36fafc7475f186adaba7eb1a2e14f24feaf
Graham Wakefield <grrrwaaa@gmail.com>
2018-07-15 13:20:04 +0900

    calibration

--- project.cpp
+++ project.cpp
@@ -663,12 +663,11 @@ void State::sim_update(float dt) {
 					// skip OOB locations:
 					if (
 						depth0[i] <= 0 
-						//|| glm::length(uv) > 0.5f
 						|| pt.x < world_min.x
 						|| pt.z < world_min.z
 						|| pt.x > world_max.x
 						|| pt.z > world_max.z
-						//|| pt.y > 3.
+						|| pt.y > (3. * kinect2world_scale)
 						) continue;
 
 					// find nearest land cell for this point:

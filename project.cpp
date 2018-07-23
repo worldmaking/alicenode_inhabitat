57b6aeeb25169d8fc9e6d10d148bcdb9f417aa0a
Graham Wakefield <grrrwaaa@gmail.com>
2018-07-23 18:30:16 +0900

    shadowing

--- project.cpp
+++ project.cpp
@@ -535,7 +535,7 @@ void State::fields_update(float dt) {
 				float hum = hu * field2world_scale - coastline_height;
 				float hlm = hum - hm;
 				float dst = C;
-				if (hm <= 0 || hlm > 1.) {
+				if (hm <= 0 || hlm > 1.5) {
 					// force lowlands to be vacant
 					// (note, human will also do this)
 					dst = 0;
